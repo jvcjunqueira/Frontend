574d9abd8390429abe2fba217dad912b8e3ffb93
jvcjunqueira <jj253606@alunos.unisanta>
2026-04-25 17:47:29 -0300

    PreparandoDeploy_FE

--- src/app/app.ts
+++ src/app/app.ts
@@ -15,7 +15,7 @@ export class App {
   apiURL : string;
 
  constructor(private http: HttpClient) {
-  this.apiURL = 'http://localhost:3000';
+  this.apiURL = 'http://backend-208s.onrender.com/api';
   this.READ_tarefas();
  }
  
